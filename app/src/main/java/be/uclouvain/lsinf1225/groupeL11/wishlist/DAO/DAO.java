88d09e776d5fb7095998a11b34fd4a6fcbd4f528
nnepper <nathan.nepper@student.uclouvain.be>
2020-04-11 16:19:57 +0200

    Initialisation getter DAO

--- app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/DAO/DAO.java
+++ app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/DAO/DAO.java
@@ -1,4 +1,76 @@
 package be.uclouvain.lsinf1225.groupeL11.wishlist.DAO;
 
+import be.uclouvain.lsinf1225.groupeL11.wishlist.Backend.*;
+import java.util.ArrayList;
+
 public class DAO {
+
+    public DAO(){
+        //TODO
+    }
+
+    public static boolean checkExistence(String emailAddress) {
+        //TODO
+        return true;
+    }
+
+    public static boolean checkPassword(String emailAddress, String password) {
+        //TODO
+        return true;
+    }
+
+    public static String getUsername(String emailAddress) {
+        //TODO
+        return null;
+    }
+
+    public static String getFirsName(String emailAddress) {
+        //TODO
+        return null;
+    }
+
+    public static String getLastName(String emailAddress) {
+        //TODO
+        return null;
+    }
+
+    public static String color(String emailAddress) {
+        //TODO
+        return null;
+    }
+
+    public static String tshirtSize(String emailAddress) {
+        //TODO
+        return null;
+    }
+
+    public static String shoesSize(String emailAddress) {
+        //TODO
+        return null;
+    }
+
+    public static String address(String emailAddress) {
+        //TODO
+        return null;
+    }
+
+    public static String getPrivacy(String emailAddress) {
+        //TODO
+        return null;
+    }
+
+    public static ArrayList<Interest> getInterests(String emailAddress) {
+        //TODO
+        return null;
+    }
+
+    public static ArrayList<User> following(String emailAddress) {
+        //TODO
+        return null;
+    }
+
+    public static ArrayList<WishList> wishLists(String emailAddress) {
+        //TODO
+        return null;
+    }
 }
