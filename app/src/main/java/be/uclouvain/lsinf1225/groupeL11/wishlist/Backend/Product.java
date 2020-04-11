7682d21763356829ed2b35d170970df04d92551d
Julesuisse <jules.lesuisse@student.uclouvain.be>
2020-04-11 15:41:01 +0200

    Implémentation de la classe Product

--- app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Backend/Product.java
+++ app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Backend/Product.java
@@ -1,4 +1,65 @@
 package be.uclouvain.lsinf1225.groupeL11.wishlist.Backend;
 
 public class Product {
+    private String productName;
+    private String productDescription;
+    private String webLink;
+    private String productPicture;
+    private String shipping;
+    private int position;
+    private int quantity;
+
+    public Product(String productName, String productDescription, String webLink, String productPicture, String shipping, int position, int quantity) {
+        this.productName = productName;
+        this.productDescription = productDescription;
+        this.webLink = webLink;
+        this.productPicture = productPicture;
+        this.shipping = shipping;
+        this.position = position;
+        this.quantity = quantity;
+    }
+
+    public void setWebLink(String webLink) {
+        this.webLink = webLink;
+    }
+
+    public void setShipping(String shipping) {
+        this.shipping = shipping;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public String getWebLink() {
+        return webLink;
+    }
+
+    public String getProductPicture() {
+        return productPicture;
+    }
+
+    public String getShipping() {
+        return shipping;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
 }
