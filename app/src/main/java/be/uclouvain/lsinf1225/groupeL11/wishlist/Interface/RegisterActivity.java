c6b26aef494bc4bec0949599ae71d54f78dd34df
GwendalLaurent <gwendal.laurent@student.uclouvain.be>
2020-04-11 23:07:54 +0200

    ajout de la registerActivity + fix des hint sur les zones de texte

--- app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Interface/RegisterActivity.java
+++ app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Interface/RegisterActivity.java
@@ -4,16 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import be.uclouvain.lsinf1225.groupeL11.wishlist.R;
-
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        // getSupportActionBar().hide(); remove top bar
-
+        setContentView(R.layout.activity_register);
     }
 }
