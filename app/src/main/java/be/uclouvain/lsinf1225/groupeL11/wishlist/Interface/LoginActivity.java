0e1fecde0b7ba410e367ef86f9e86037313af992
nnepper <nathan.nepper@student.uclouvain.be>
2020-04-11 14:53:17 +0200

    Refactorisation sous forme de package

--- app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Interface/LoginActivity.java
+++ app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Interface/LoginActivity.java
@@ -1,8 +1,10 @@
-package be.uclouvain.lsinf1225.groupeL11.wishlist;
+package be.uclouvain.lsinf1225.groupeL11.wishlist.Interface;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import be.uclouvain.lsinf1225.groupeL11.wishlist.R;
 
 public class LoginActivity extends AppCompatActivity {
 
