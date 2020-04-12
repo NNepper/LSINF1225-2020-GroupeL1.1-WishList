7d6221a19f8ebe7282e398c38e23fd3233417088
GwendalLaurent <gwendal.laurent@student.uclouvain.be>
2020-04-12 17:28:05 +0200

    RegisterActivity interface + add backArrow to get back to mainActivity

--- app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Interface/LoginActivity.java
+++ app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Interface/LoginActivity.java
@@ -2,11 +2,16 @@ package be.uclouvain.lsinf1225.groupeL11.wishlist.Interface;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import be.uclouvain.lsinf1225.groupeL11.wishlist.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ImageView backArrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +20,15 @@ public class LoginActivity extends AppCompatActivity {
 
         // getSupportActionBar().hide(); remove top bar
 
+        this.backArrow = (ImageView) findViewById(R.id.BackHomeArrow);
+
+        this.backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent back_to_home_page = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(back_to_home_page);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Interface/RegisterActivity.java
+++ app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Interface/RegisterActivity.java
@@ -2,15 +2,31 @@ package be.uclouvain.lsinf1225.groupeL11.wishlist.Interface;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import be.uclouvain.lsinf1225.groupeL11.wishlist.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private ImageView backArrow;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        this.backArrow = (ImageView) findViewById(R.id.BackHomeArrow);
+
+        this.backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent back_to_home_page = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(back_to_home_page);
+                finish();
+            }
+        });
     }
 }
