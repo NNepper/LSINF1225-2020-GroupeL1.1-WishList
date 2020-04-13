1a22e1181071fa35959e60c71e5cd429f50beaf7
Julesuisse <jules.lesuisse@student.uclouvain.be>
2020-04-13 11:39:57 +0200

    Changement vers DAO de WishList

--- app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Backend/WishList.java
+++ app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Backend/WishList.java
@@ -1,52 +1,35 @@
 package be.uclouvain.lsinf1225.groupeL11.wishlist.Backend;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 
+import be.uclouvain.lsinf1225.groupeL11.wishlist.DAO.WishListDAO;
+
 public class WishList {
-    private String wishlistName;
-    private String wishlistDescrpition;
-    private ArrayList<Product> products;
+    ArrayList<Product> products;
 
-    public WishList(String wishlistName, String wishlistDecrpition, ArrayList<Product> products) {
-        this.wishlistName = wishlistName;
-        this.wishlistDescrpition = wishlistDecrpition;
-        this.products = products;
+    public WishList() {
+        //TODO
     }
 
-    public String getWishlistName() {
-        return wishlistName;
+    public String getWishListName() { return WishListDAO.getWishListName(this); }
+
+    public String getWishListDescription() {
+        return WishListDAO.getWishListDescription(this);
     }
 
-    public String getWishlistDecrpition() {
-        return wishlistDescrpition;
-    }
+    public ArrayList<Product> getProducts() { return WishListDAO.getProducts(this); }
 
-    public ArrayList<Product> getProducts() {
-        return products;
-    }
+    public Product getProduct(int i) { return WishListDAO.getProduct(i, this); }
 
-    public Product getProduct(int i) {
-        return products.get(i);
-    }
+    public void addProduct(Product product) { WishListDAO.addProduct(product, this); }
 
-    public void addProduct(Product product) {
-        products.add(product);
-    }
+    public void addProduct(int i, Product product) { WishListDAO.addProduct(i, product, this); }
 
-    public void addProduct(int i, Product product) {
-        products.add(i, product);
-    }
+    public boolean removeProduct(Product product) { return WishListDAO.removeProduct(product, this); }
 
-    public boolean removeProduct(Product product) {
-        return products.remove(product);
-    }
-
-    public Product removeProduct(int i) {
-        return products.remove(i);
-    }
+    public Product removeProduct(int i) { return WishListDAO.removeProduct(i, this); }
 
     public void sortProductsByPosition() {
         Collections.sort(products, new Comparator<Product>() {
--- app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/DAO/WishListDAO.java
+++ app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/DAO/WishListDAO.java
@@ -1,4 +1,54 @@
 package be.uclouvain.lsinf1225.groupeL11.wishlist.DAO;
 
+import java.util.ArrayList;
+
+import be.uclouvain.lsinf1225.groupeL11.wishlist.Backend.Product;
+import be.uclouvain.lsinf1225.groupeL11.wishlist.Backend.WishList;
+
 public class WishListDAO {
+
+    public WishListDAO() {
+        //TODO
+    }
+
+    public static String getWishListName(WishList wishList) {
+        //TODO
+        return null;
+    }
+
+    public static String getWishListDescription(WishList wishList) {
+        //TODO
+        return null;
+    }
+
+    public static ArrayList<Product> getProducts(WishList wishList) {
+        //TODO
+        return null;
+    }
+
+    public static Product getProduct(int i, WishList wishList) {
+        //TODO
+        return null;
+    }
+
+    public static void addProduct(Product product, WishList wishList) {
+        //TODO
+        return;
+    }
+
+    public static void addProduct(int i, Product product, WishList wishList) {
+        //TODO
+        return;
+    }
+
+    public static boolean removeProduct(Product product, WishList wishList) {
+        //TODO
+        return true;
+    }
+
+    public static Product removeProduct(int i, WishList wishList) {
+        //TODO
+        return null;
+    }
+
 }
