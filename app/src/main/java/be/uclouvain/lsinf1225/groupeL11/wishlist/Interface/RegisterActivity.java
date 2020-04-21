012fe41533b91a6197e06b70fbea597a9f119354
nnepper <nathan.nepper@student.uclouvain.be>
2020-04-21 16:06:41 +0200

    Implémentation OnClickListenner Register

--- app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Interface/RegisterActivity.java
+++ app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Interface/RegisterActivity.java
@@ -5,8 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import be.uclouvain.lsinf1225.groupeL11.wishlist.Backend.User;
+import be.uclouvain.lsinf1225.groupeL11.wishlist.DAO.UserDAO;
 import be.uclouvain.lsinf1225.groupeL11.wishlist.R;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -23,9 +31,43 @@ public class RegisterActivity extends AppCompatActivity {
         this.backArrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent back_to_home_page = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(back_to_home_page);
-                finish();
+                EditText editText1 = (EditText) findViewById(R.id.Username);
+                EditText editText2 = (EditText) findViewById(R.id.Password);
+                EditText editText3 = (EditText) findViewById(R.id.ConfirmPassword);
+                EditText editText4 = (EditText) findViewById(R.id.Email);
+
+                String username = editText1.getText().toString();
+                String password = editText2.getText().toString();
+                String confPassword = editText3.getText().toString();
+                String email = editText4.getText().toString();
+
+                if (password == confPassword) {
+                    User mainUser = new User(-1);
+                    mainUser.firstname = null;
+                    mainUser.lastname = null;
+                    mainUser.email = email;
+                    mainUser.password = password;
+                    mainUser.username = username;
+                    mainUser.color = null;
+                    mainUser.tshirtSize = null;
+                    mainUser.trouserSize = null;
+                    mainUser.shoeSize = 0;
+                    mainUser.address = null;
+
+                    // Bundle for easy Object storage
+                    Bundle data = new Bundle();
+                    data.putSerializable("mainUser", (Serializable) mainUser);
+
+                    // Start new Activity and pass data to the next Activity
+                    Intent back_to_home_page = new Intent(getApplicationContext(), MainActivity.class);
+                    back_to_home_page.putExtras(data);
+                    startActivity(back_to_home_page);
+                    finish();
+                }
+                else {
+                    // Send message: "The two passwords aren't the same"
+                    return;
+                }
             }
         });
     }
