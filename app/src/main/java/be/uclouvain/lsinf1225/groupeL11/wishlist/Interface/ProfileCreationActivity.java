f791264370a6fca17eca59b5c456f82de58bc21d
WhippedCoconut <corentin.lengele@student.uclouvain.be>
2020-04-23 13:21:22 +0200

    Interface: added adapter for dropdown buttons

--- app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Interface/ProfileCreationActivity.java
+++ app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Interface/ProfileCreationActivity.java
@@ -9,22 +9,22 @@ import android.widget.Spinner;
 
 public class ProfileCreationActivity extends AppCompatActivity {
 
-    Spinner drowndownColor;
+    Spinner dropdownColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_creation);
 
-        drowndownColor = findViewById(R.id.newprofile_color);
-        String[] testItems = {"Red", "Blue", "Green"};
+        dropdownColor = findViewById(R.id.newprofile_color);
+        String[] testItems = {"Choose color", "Color: Red", "Color: Blue", "Color: Green"};
 
 
         //TODO: adapter layout for dropdown
-        drowndownColor.setAdapter(new ArrayAdapter<String>(
+        dropdownColor.setAdapter(new ArrayAdapter<String>(
                 this,
-                R.layout.adapter_wishlist_item,
-                R.id.item_wishlist_title,
+                R.layout.adapter_dropdown,
+                R.id.dropdown_title,
                 testItems
         ));
 
