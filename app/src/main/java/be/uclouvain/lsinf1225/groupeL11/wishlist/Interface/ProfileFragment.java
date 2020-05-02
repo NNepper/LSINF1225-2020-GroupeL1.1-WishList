85cd10164af6adf86fa23fe8d9f087fc7804c2fc
GwendalLaurent <gwendal.laurent@student.uclouvain.be>
2020-05-02 22:42:36 +0200

    profile intent updated logOut and validate buttons added

--- app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Interface/ProfileFragment.java
+++ app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Interface/ProfileFragment.java
@@ -1,21 +1,56 @@
 package be.uclouvain.lsinf1225.groupeL11.wishlist.Interface;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import be.uclouvain.lsinf1225.groupeL11.wishlist.Backend.User;
 import be.uclouvain.lsinf1225.groupeL11.wishlist.R;
 
 public class ProfileFragment extends Fragment {
 
+    private TextView usernameTextView;
+    private ImageView logOut;
+    private ImageView confirm;
+    private User mainUser;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home_profile, container, false);
+        this.mainUser = ((HomeActivity) getActivity()).mainUser;
+        final View view = inflater.inflate(R.layout.fragment_home_profile, container, false);
+
+        usernameTextView = view.findViewById(R.id.profileUsername);
+        usernameTextView.setText(mainUser.username);
+
+        logOut = view.findViewById(R.id.profileLogOut);
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Maybe use a dialog to avoid miss click ?
+                Intent back_to_main_page = new Intent(getActivity().getApplicationContext(), MainActivity.class);
+                startActivity(back_to_main_page);
+                getActivity().finish();
+            }
+        });
+
+        confirm = view.findViewById(R.id.profileConfirm);
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO get all infos inside textViews and update them inside DB with DAO
+                // TODO when update is done reaload the view with new infos
+            }
+        });
+
+        return view;
     }
 }
