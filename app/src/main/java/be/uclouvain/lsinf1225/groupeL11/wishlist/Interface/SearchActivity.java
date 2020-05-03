10060499b922b797581d315a4d4d346eb09c87a9
Julesuisse <jules.lesuisse@student.uclouvain.be>
2020-05-03 18:47:53 +0200

    Avancement de la fonction doMySearch()

--- app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Interface/SearchActivity.java
+++ app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Interface/SearchActivity.java
@@ -1,14 +1,26 @@
 package be.uclouvain.lsinf1225.groupeL11.wishlist.Interface;
 
+import android.app.ListActivity;
 import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
+import be.uclouvain.lsinf1225.groupeL11.wishlist.Backend.User;
+import be.uclouvain.lsinf1225.groupeL11.wishlist.DAO.UserDAO;
 import be.uclouvain.lsinf1225.groupeL11.wishlist.R;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends ListActivity {
+
+    private Bundle data;
+    public User mainUser;
+    UserDAO userDAO;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,8 +35,29 @@ public class SearchActivity extends AppCompatActivity {
         }
     }
 
-    private void doMySearch(String query) {
+    private Adapter doMySearch(String query) {
         // TODO for return type see : https://developer.android.com/guide/topics/search/search-dialog#PresentingTheResults
+        Context context = getApplicationContext();
+        if (userDAO == null){
+            userDAO = new UserDAO(context);
+        }
+        this.data = getIntent().getExtras(); // getting bundle from other Activity
+        this.mainUser = data.getParcelable(getApplicationContext().getString(R.string.mainUserBundleParcable)); // get string form string.xml
+
+        ArrayList<User> users = userDAO.getAllUser(mainUser.id);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                getUsername(users) );
+        return arrayAdapter;
+    }
+
+    private ArrayList<String> getUsername(ArrayList<User> users) {
+        ArrayList<String> usernames = new ArrayList<>();
+        for (int i = 0; i < users.size(); i++) {
+            usernames.add(users.get(i).username);
+        }
+        return usernames;
     }
 
 }
