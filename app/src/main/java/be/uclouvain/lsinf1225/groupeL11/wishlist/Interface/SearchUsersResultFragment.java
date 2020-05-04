c42199811941dea8b0a4d8d96c59f777af403928
Julesuisse <jules.lesuisse@student.uclouvain.be>
2020-05-05 01:04:52 +0200

    No changes boyzzzz

--- app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Interface/SearchUsersResultFragment.java
+++ app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Interface/SearchUsersResultFragment.java
@@ -2,16 +2,20 @@ package be.uclouvain.lsinf1225.groupeL11.wishlist.Interface;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
