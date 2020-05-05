7d6d34088cda498e8653deec9dbc940226812c01
nnepper <nathan.nepper@student.uclouvain.be>
2020-05-05 23:04:19 +0200

    Modification getFollowing pour prise en compte pending

--- app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/DAO/FollowDAO.java
+++ app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/DAO/FollowDAO.java
@@ -25,7 +25,7 @@ public class FollowDAO extends MyDatabaseHelper {
         try {
             String getQuery = String.format(
                     "SELECT * FROM User_has_Friends uhf " +
-                            "WHERE uhf.userID == '%s'", userID);
+                            "WHERE uhf.userID == '%s' AND uhf.pending == 0", userID);
 
             Cursor cursor = db.rawQuery(getQuery, null);
             if (cursor.moveToFirst()) {
