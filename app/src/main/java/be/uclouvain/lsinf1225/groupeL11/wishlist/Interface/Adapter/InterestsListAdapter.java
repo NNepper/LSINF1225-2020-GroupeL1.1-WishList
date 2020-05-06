d3301a439d7625028a41b7781d21aa01d338e074
Julesuisse <jules.lesuisse@student.uclouvain.be>
2020-05-07 00:45:04 +0200

    Ajout de l'affichage des intérets mais non fonctionnel

--- app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Interface/Adapter/InterestsListAdapter.java
+++ app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Interface/Adapter/InterestsListAdapter.java
@@ -1,4 +1,89 @@
 package be.uclouvain.lsinf1225.groupeL11.wishlist.Interface.Adapter;
 
-public class InterestListAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import be.uclouvain.lsinf1225.groupeL11.wishlist.Backend.Interest;
+import be.uclouvain.lsinf1225.groupeL11.wishlist.R;
+
+public class InterestsListAdapter extends RecyclerView.Adapter<InterestsListAdapter.InterestsViewHolder> {
+    private ArrayList<Interest> interestsList;
+    private InterestsListAdapter.onItemClickListener interestsClickListener;
+
+    public interface onItemClickListener{
+        void onItemClick(int position);
+        void onCheckClick(int position);
+    }
+
+    public void setOnItemClickLister(InterestsListAdapter.onItemClickListener listener){
+        interestsClickListener = listener;
+    }
+
+    public static class InterestsViewHolder extends RecyclerView.ViewHolder{
+        public CheckBox checkBox;
+        public TextView interestName;
+
+        public InterestsViewHolder(@NonNull View itemView, final InterestsListAdapter.onItemClickListener listener) {
+            super(itemView);
+            interestName = itemView.findViewById(R.id.interest_name);
+            checkBox = itemView.findViewById(R.id.interest_item_checkbox);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null){
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION){
+                            listener.onItemClick(position);
+                        }
+                    }
+                }
+            });
+
+            checkBox.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) { // TODO changer pour pouvoir ajouter l'user à la liste du main
+                    if (listener != null){
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION){
+                            listener.onCheckClick(position);
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    public InterestsListAdapter(ArrayList<Interest> interests){
+        this.interestsList = interests;
+    }
+
+    @NonNull
+    @Override
+    public InterestsListAdapter.InterestsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_interest_item, parent, false);
+        InterestsListAdapter.InterestsViewHolder flvh = new InterestsListAdapter.InterestsViewHolder(view, interestsClickListener);
+        return flvh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull InterestsListAdapter.InterestsViewHolder holder, int position) {
+        Interest currentInterest = interestsList.get(position);
+
+        holder.interestName.setText(currentInterest.getInterestName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return interestsList.size();
+    }
 }
--- app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Interface/InterestsFragment.java
+++ app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Interface/InterestsFragment.java
@@ -1,4 +1,77 @@
 package be.uclouvain.lsinf1225.groupeL11.wishlist.Interface;
 
-public class InterestsFragment {
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import be.uclouvain.lsinf1225.groupeL11.wishlist.Backend.Interest;
+import be.uclouvain.lsinf1225.groupeL11.wishlist.Backend.User;
+import be.uclouvain.lsinf1225.groupeL11.wishlist.DAO.InterestDAO;
+import be.uclouvain.lsinf1225.groupeL11.wishlist.Interface.Adapter.InterestsListAdapter;
+import be.uclouvain.lsinf1225.groupeL11.wishlist.Interface.Adapter.SearchUsersResultAdapter;
+import be.uclouvain.lsinf1225.groupeL11.wishlist.R;
+
+public class InterestsFragment extends Fragment {
+
+    private RecyclerView interestsListRecyclerView;
+    private InterestsListAdapter interestsListAdapter;
+    private RecyclerView.LayoutManager interestsListLayoutManager;
+    private User mainUser;
+    private ImageView backArrow;
+
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        this.mainUser = ((HomeActivity) getActivity()).mainUser;
+        InterestDAO interestDAO = new InterestDAO(getContext());
+        final ArrayList<Interest> interestsList = interestDAO.getAllInterests();
+        final View view = inflater.inflate(R.layout.fragment_interests, container, false);
+        this.backArrow = (ImageView) view.findViewById(R.id.interests_back_arrow);
+
+        interestsListRecyclerView = view.findViewById(R.id.interests_recycler_view);
+        interestsListRecyclerView.setHasFixedSize(true);
+        interestsListLayoutManager = new LinearLayoutManager(view.getContext());
+        interestsListAdapter = new InterestsListAdapter(interestsList);
+
+        this.backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Status", "Clicked !!");
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, new ProfileFragment()).commit();
+            }
+        });
+
+        interestsListRecyclerView.setLayoutManager(interestsListLayoutManager);
+        interestsListRecyclerView.setAdapter(interestsListAdapter);
+
+        interestsListAdapter.setOnItemClickLister(new InterestsListAdapter.onItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+
+            }
+
+            @Override
+            public void onCheckClick(int position) {
+                // TODO gérer ce qu'il se passe quand on check la box !!!
+            }
+        });
+
+        return view;
+    }
 }
