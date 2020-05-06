5f5ad8ec870ccd0016169853050b6f4807d5c6fa
nnepper <nathan.nepper@student.uclouvain.be>
2020-05-07 01:24:49 +0200

    Fix problème import image

--- app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Interface/ProfileFragment.java
+++ app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Interface/ProfileFragment.java
@@ -234,7 +234,6 @@ public class ProfileFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-                photoPickerIntent.setType("image/*");
                 startActivityForResult(photoPickerIntent, RESULT_LOAD_IMG);
             }
         });
