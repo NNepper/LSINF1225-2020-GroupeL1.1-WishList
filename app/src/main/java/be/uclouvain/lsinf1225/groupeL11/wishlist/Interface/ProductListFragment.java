10ac2a33a0a2703fb4234deee866622ba0225b50
GwendalLaurent <gwendal.laurent@student.uclouvain.be>
2020-05-08 02:52:18 +0200

    not letting user creating a new item without entering all data needed

--- app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Interface/ProductListFragment.java
+++ app/src/main/java/be/uclouvain/lsinf1225/groupeL11/wishlist/Interface/ProductListFragment.java
@@ -157,13 +157,17 @@ public class ProductListFragment extends Fragment {
                         .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                Product newProduct = new Product(itemName.getText().toString(), Integer.parseInt(itemQuantity.getText().toString()), itemWebLink.getText().toString(), products.size());
-                                newProduct.wishlist = wishList;
-                                if (productDAO.create(newProduct)){
-                                    products.add(newProduct);
-                                    productItemAdapter.notifyItemInserted(newProduct.position);
-                                }else{
-                                   showToast("Error while trying to create a new item");
+                                if(itemName.getText().toString().length() == 0 || itemQuantity.getText().toString().length() == 0 || itemWebLink.getText().toString().length() == 0){
+                                    showToast("Please enter informations");
+                                }else {
+                                    Product newProduct = new Product(itemName.getText().toString(), Integer.parseInt(itemQuantity.getText().toString()), itemWebLink.getText().toString(), products.size());
+                                    newProduct.wishlist = wishList;
+                                    if (productDAO.create(newProduct)) {
+                                        products.add(newProduct);
+                                        productItemAdapter.notifyItemInserted(newProduct.position);
+                                    } else {
+                                        showToast("Error while trying to create a new item");
+                                    }
                                 }
                             }
                         }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
